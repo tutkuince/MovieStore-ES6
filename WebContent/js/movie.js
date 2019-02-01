e59cbfe156a91f53eaf15e2596d34b738292beb9
tutkuince <tutku.ince@outlook.com>
2019-02-01 19:46:58 +0300

    movie class

---
+++ WebContent/js/movie.js
@@ -0,0 +1,14 @@
+// Movie Class
+// function Movie(title, director, link) {
+//     this.title = title;
+//     this.director = director;
+//     this.link = link;
+// }
+
+class Movie {
+    constructor(title, director, link){
+        this.title = title;
+        this.director = director;
+        this.link = link;
+    }
+}
